d90920ac8ffbd3bf15f15090d461ae22faf54f06
jaewoo4710 <jingwoo4710@nate.com>
2021-03-16 15:56:21 +0900

    update all

---
+++ streamlit_app.py
@@ -0,0 +1,439 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This demo lets you to explore the Udacity self-driving car image dataset.
+# More info: https://github.com/streamlit/demo-self-driving
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
+import os, urllib, cv2
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+# Streamlit encourages well-structured code, like starting execution in a main() function.
+def main():
+    # Render the readme as markdown using st.markdown.
+    readme_text = st.markdown(get_file_content_as_string("instructions.md"))
+
+    # Download external dependencies.
+    for filename in EXTERNAL_DEPENDENCIES.keys():
+        download_file(filename)
+
+    # Once we have the dependencies, add a selector for the app mode on the sidebar.
+    st.sidebar.title("What to do")
+    app_mode = st.sidebar.selectbox("Choose the network",
+        ["Yolov3", "Dectron2"])
+    if app_mode == "Dectron2":
+        readme_text.empty()
+        run_the_dectron2()
+    elif app_mode == "Yolov3":
+        readme_text.empty()
+        run_the_yolo()
+
+# This file downloader demonstrates Streamlit animation.
+def download_file(file_path):
+    # Don't download the file twice. (If possible, verify the download using the file length.)
+    if os.path.exists(file_path):
+        if "size" not in EXTERNAL_DEPENDENCIES[file_path]:
+            return
+        elif os.path.getsize(file_path) == EXTERNAL_DEPENDENCIES[file_path]["size"]:
+            return
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % file_path)
+        progress_bar = st.progress(0)
+        with open(file_path, "wb") as output_file:
+            with urllib.request.urlopen(EXTERNAL_DEPENDENCIES[file_path]["url"]) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
+                        (file_path, counter / MEGABYTES, length / MEGABYTES))
+                    progress_bar.progress(min(counter / length, 1.0))
+
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+# This is the main app app itself, which appears when the user selects "Run the app".
+def run_the_yolo():
+    # To make Streamlit fast, st.cache allows us to reuse computation across runs.
+    # In this common pattern, we download data from an endpoint only once.
+    @st.cache
+    def load_metadata(url):
+        return pd.read_csv(url)
+
+    # This function uses some Pandas magic to summarize the metadata Dataframe.
+    @st.cache
+    def create_summary(metadata):
+        one_hot_encoded = pd.get_dummies(metadata[["frame", "label"]], columns=["label"])
+        summary = one_hot_encoded.groupby(["frame"]).sum().rename(columns={
+            "label_biker": "biker",
+            "label_car": "car",
+            "label_pedestrian": "pedestrian",
+            "label_trafficLight": "traffic light",
+            "label_truck": "truck"
+        })
+        return summary
+
+    # An amazing property of st.cached functions is that you can pipe them into
+    # one another to form a computation DAG (directed acyclic graph). Streamlit
+    # recomputes only whatever subset is required to get the right answer!
+    metadata = load_metadata(os.path.join(DATA_URL_ROOT, "labels.csv.gz"))
+    summary = create_summary(metadata)
+
+    # Uncomment these lines to peek at these DataFrames.
+    # st.write('## Metadata', metadata[:1000], '## Summary', summary[:1000])
+
+    # Draw the UI elements to search for objects (pedestrians, cars, etc.)
+    selected_frame_index, selected_frame = frame_selector_ui(summary)
+    if selected_frame_index == None:
+        st.error("No frames fit the criteria. Please select different label or number.")
+        return
+
+    # Draw the UI element to select parameters for the YOLO object detector.
+    confidence_threshold, overlap_threshold = object_detector_ui()
+
+    # # Load the image from S3.
+    # image_url = os.path.join(DATA_URL_ROOT, selected_frame)
+    # image = load_image(image_url)
+    # Retrieve image
+    image = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+    if image is not None:
+        try:
+            file_bytes = np.asarray(bytearray(image.read()), dtype=np.uint8)
+            img = cv2.imdecode(file_bytes, 1)
+            # Get the boxes for the objects detected by YOLO by running the YOLO model.
+            yolo_boxes = yolo_v3(img, confidence_threshold, overlap_threshold)
+            draw_image_with_boxes(img, yolo_boxes, "Real-time Computer Vision",
+            "**YOLO v3 Model** (overlap `%3.1f`) (confidence `%3.1f`)" % (overlap_threshold, confidence_threshold))
+        except:
+            st.subheader("Please, reupload an image to see the changes")
+
+    # # Get the boxes for the objects detected by YOLO by running the YOLO model.
+    # yolo_boxes = yolo_v3(image, confidence_threshold, overlap_threshold)
+    # draw_image_with_boxes(image, yolo_boxes, "Real-time Computer Vision",
+    #     "**YOLO v3 Model** (overlap `%3.1f`) (confidence `%3.1f`)" % (overlap_threshold, confidence_threshold))
+
+# This sidebar UI is a little search engine to find certain object types.
+def frame_selector_ui(summary):
+    st.sidebar.markdown("# Frame")
+
+    # The user can pick which type of object to search for.
+    object_type = st.sidebar.selectbox("Search for which objects?", summary.columns, 2)
+
+    # The user can select a range for how many of the selected objecgt should be present.
+    min_elts, max_elts = st.sidebar.slider("How many %ss (select a range)?" % object_type, 0, 25, [10, 20])
+    selected_frames = get_selected_frames(summary, object_type, min_elts, max_elts)
+    if len(selected_frames) < 1:
+        return None, None
+
+    # Choose a frame out of the selected frames.
+    selected_frame_index = st.sidebar.slider("Choose a frame (index)", 0, len(selected_frames) - 1, 0)
+
+    # Draw an altair chart in the sidebar with information on the frame.
+    objects_per_frame = summary.loc[selected_frames, object_type].reset_index(drop=True).reset_index()
+    chart = alt.Chart(objects_per_frame, height=120).mark_area().encode(
+        alt.X("index:Q", scale=alt.Scale(nice=False)),
+        alt.Y("%s:Q" % object_type))
+    selected_frame_df = pd.DataFrame({"selected_frame": [selected_frame_index]})
+    vline = alt.Chart(selected_frame_df).mark_rule(color="red").encode(x = "selected_frame")
+    st.sidebar.altair_chart(alt.layer(chart, vline))
+
+    selected_frame = selected_frames[selected_frame_index]
+    return selected_frame_index, selected_frame
+
+# Select frames based on the selection in the sidebar
+@st.cache(hash_funcs={np.ufunc: str})
+def get_selected_frames(summary, label, min_elts, max_elts):
+    return summary[np.logical_and(summary[label] >= min_elts, summary[label] <= max_elts)].index
+
+# This sidebar UI lets the user select parameters for the YOLO object detector.
+def object_detector_ui():
+    st.sidebar.markdown("# Model")
+    confidence_threshold = st.sidebar.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.01)
+    overlap_threshold = st.sidebar.slider("Overlap threshold", 0.0, 1.0, 0.3, 0.01)
+    return confidence_threshold, overlap_threshold
+
+# Draws an image with boxes overlayed to indicate the presence of cars, pedestrians etc.
+def draw_image_with_boxes(image, boxes, header, description):
+    # Superpose the semi-transparent object detection boxes.    # Colors for the boxes
+    LABEL_COLORS = {
+        "car": [255, 0, 0],
+        "pedestrian": [0, 255, 0],
+        "truck": [0, 0, 255],
+        "trafficLight": [255, 255, 0],
+        "biker": [255, 0, 255],
+    }
+    image_with_boxes = image.astype(np.float64)
+    for _, (xmin, ymin, xmax, ymax, label) in boxes.iterrows():
+        image_with_boxes[int(ymin):int(ymax),int(xmin):int(xmax),:] += LABEL_COLORS[label]
+        image_with_boxes[int(ymin):int(ymax),int(xmin):int(xmax),:] /= 2
+
+    # Draw the header and image.
+    st.subheader(header)
+    st.markdown(description)
+    st.image(image_with_boxes.astype(np.uint8), use_column_width=True)
+
+# Download a single file and make its content available as a string.
+@st.cache(show_spinner=False)
+def get_file_content_as_string(path):
+    url = 'https://raw.githubusercontent.com/streamlit/demo-self-driving/master/' + path
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf-8")
+
+# This function loads an image from Streamlit public repo on S3. We use st.cache on this
+# function as well, so we can reuse the images across runs.
+@st.cache(show_spinner=False)
+def load_image(url):
+    with urllib.request.urlopen(url) as response:
+        image = np.asarray(bytearray(response.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = image[:, :, [2, 1, 0]] # BGR -> RGB
+    return image
+
+# Run the YOLO model to detect objects.
+def yolo_v3(image, confidence_threshold, overlap_threshold):
+    # Load the network. Because this is cached it will only happen once.
+    @st.cache(allow_output_mutation=True)
+    def load_network(config_path, weights_path):
+        net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
+        output_layer_names = net.getLayerNames()
+        output_layer_names = [output_layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        return net, output_layer_names
+    net, output_layer_names = load_network("yolov3.cfg", "yolov3.weights")
+
+    # Run the YOLO neural net.
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layer_outputs = net.forward(output_layer_names)
+
+    # Supress detections in case of too low confidence or too much overlap.
+    boxes, confidences, class_IDs = [], [], []
+    H, W = image.shape[:2]
+    for output in layer_outputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > confidence_threshold:
+                box = detection[0:4] * np.array([W, H, W, H])
+                centerX, centerY, width, height = box.astype("int")
+                x, y = int(centerX - (width / 2)), int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                class_IDs.append(classID)
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, overlap_threshold)
+
+    # Map from YOLO labels to Udacity labels.
+    UDACITY_LABELS = {
+        0: 'pedestrian',
+        1: 'biker',
+        2: 'car',
+        3: 'biker',
+        5: 'truck',
+        7: 'truck',
+        9: 'trafficLight'
+    }
+    xmin, xmax, ymin, ymax, labels = [], [], [], [], []
+    if len(indices) > 0:
+        # loop over the indexes we are keeping
+        for i in indices.flatten():
+            label = UDACITY_LABELS.get(class_IDs[i], None)
+            if label is None:
+                continue
+
+            # extract the bounding box coordinates
+            x, y, w, h = boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]
+
+            xmin.append(x)
+            ymin.append(y)
+            xmax.append(x+w)
+            ymax.append(y+h)
+            labels.append(label)
+
+    boxes = pd.DataFrame({"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax, "labels": labels})
+    return boxes[["xmin", "ymin", "xmax", "ymax", "labels"]]
+
+@st.cache(persist=True)
+def initialization():
+    """Loads configuration and model for the prediction.
+    
+    Returns:
+        cfg (detectron2.config.config.CfgNode): Configuration for the model.
+        classes_names (list: str): Classes available for the model of interest.
+        predictor (detectron2.engine.defaults.DefaultPredicto): Model to use.
+            by the model.
+        
+    """
+    cfg = get_cfg()
+    # Force model to operate within CPU, erase if CUDA compatible devices ara available
+    cfg.MODEL.DEVICE = 'cpu'
+    # Add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    # Set threshold for this model
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    # Get classes names for the dataset of interest
+    classes_names = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes
+    # Initialize prediction model
+    predictor = DefaultPredictor(cfg)
+
+    return cfg, classes_names, predictor
+
+
+def inference(predictor, img):
+    return predictor(img)
+
+
+@st.cache
+def output_image(cfg, img, outputs):
+    v = Visualizer(img[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    processed_img = out.get_image() #[:, :, ::-1]
+
+    return processed_img
+
+
+#@st.cache
+def retrieve_image():
+    uploaded_img = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+    uploaded_img_cache = None
+    if uploaded_img is not None and uploaded_img != uploaded_img_cache:
+        uploaded_img_cache = uploaded_img
+        print(uploaded_img)
+        return image(uploaded_img)
+        # file_bytes = np.asarray(bytearray(uploaded_img.read()), dtype=np.uint8)
+        # try:
+        #     img = cv2.imdecode(file_bytes, 1)
+        # except:
+        #     pass
+        # return img
+    
+
+@st.cache
+def image(uploaded_img):
+    file_bytes = np.asarray(bytearray(uploaded_img.read()), dtype=np.uint8)
+    try:
+        img = cv2.imdecode(file_bytes, 1)
+        return img
+    except:
+        return None
+    return img
+
+
+
+@st.cache
+def discriminate(outputs, classes_to_detect):
+    """Select which classes to detect from an output.
+
+    Get the dictionary associated with the outputs instances and modify
+    it according to the given classes to restrict the detection to them
+
+    Args:
+        outputs (dict):
+            instances (detectron2.structures.instances.Instances): Instance
+                element which contains, among others, "pred_boxes", 
+                "pred_classes", "scores" and "pred_masks".
+        classes_to_detect (list: int): Identifiers of the dataset on which
+            the model was trained.
+
+    Returns:
+        ouputs (dict): Same dict as before, but modified to match
+            the detection classes.
+
+    """
+    pred_classes = np.array(outputs['instances'].pred_classes)
+    # Take the elements matching *classes_to_detect*
+    mask = np.isin(pred_classes, classes_to_detect)
+    # Get the indexes
+    idx = np.nonzero(mask)
+
+    # Get Instance values as a dict and leave only the desired ones
+    out_fields = outputs['instances'].get_fields()
+    for field in out_fields:
+        out_fields[field] = out_fields[field][idx]
+
+    return outputs
+
+
+def run_the_dectron2():
+    # Initialization
+    cfg, classes, predictor = initialization()
+
+    # Streamlit initialization
+    st.title("Instance Segmentation")
+    st.sidebar.title("Options")
+    ## Select classes to be detected by the model
+    classes_to_detect = st.sidebar.multiselect(
+        "Select which classes to detect", classes, ['person'])
+    mask = np.isin(classes, classes_to_detect)
+    class_idxs = np.nonzero(mask)
+
+    # Define holder for the processed image
+    img_placeholder = st.empty()
+
+    # Retrieve image
+    uploaded_img = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+    if uploaded_img is not None:
+        try:
+            file_bytes = np.asarray(bytearray(uploaded_img.read()), dtype=np.uint8)
+            img = cv2.imdecode(file_bytes, 1)
+            # Detection code
+            outputs = inference(predictor, img)
+            outputs = discriminate(outputs, class_idxs)
+            out_image = output_image(cfg, img, outputs)
+            st.image(out_image, caption='Processed Image', use_column_width=True)        
+        except:
+            st.subheader("Please, reupload an image to see the changes")
+
+
+# Path to the Streamlit public S3 bucket
+DATA_URL_ROOT = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
+
+# External files to download.
+EXTERNAL_DEPENDENCIES = {
+    "yolov3.weights": {
+        "url": "https://pjreddie.com/media/files/yolov3.weights",
+        "size": 248007048
+    },
+    "yolov3.cfg": {
+        "url": "https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg",
+        "size": 8342
+    }
+}
+
+if __name__ == "__main__":
+    main()
